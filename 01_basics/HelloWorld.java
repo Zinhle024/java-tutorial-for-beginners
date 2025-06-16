2ba60c2af88afe058591d84065032832fb5bcbac
Zinhle Mthimunye <zimthimjhb024@student.wethinkcode.co.za>
2025-06-16 12:07:41 +0200

    Added HelloWorld.java in 01_basics folder to test setup

---
+++ 01_basics/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    //My first java program to test setup and printing output
+    public static void main ( String[] args) {
+        System.out.println("Hi my name is Zinhle and I am learning Java!");
+    }
+}
